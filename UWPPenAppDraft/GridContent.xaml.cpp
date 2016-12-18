a5091ff9748e9d1f31bc3ac00616e5682bd8e18b
MinSikMoon <msmoon.business@gmail.com>
2016-12-18 22:18:39 +0900

    1st: uwp 순서도 프로그래밍

---
+++ UWPPenAppDraft/GridContent.xaml.cpp
@@ -0,0 +1,209 @@
+﻿//
+// GridContent.xaml.cpp
+// Implementation of the GridContent class
+//
+
+#include "pch.h"
+#include "GridContent.xaml.h"
+
+using namespace UWPPenAppDraft;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::ApplicationModel::DataTransfer;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+GridContent::GridContent()
+{
+	InitializeComponent();
+}
+void  UWPPenAppDraft::GridContent::Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	makeGridArray(50, 50, 100);
+	////1. 8개의 행을 그린다. 
+	//for (int i = 0; i < 8; i++) {
+	//	RowDefinition^ tempRow = ref new RowDefinition();
+	//	tempRow->Height = 100;
+	//	PageGrid->RowDefinitions->Append(tempRow);
+	//}
+
+	////2. 8개의 열을 그린다. 
+	//for (int i = 0; i < 8; i++) {
+	//	ColumnDefinition^ tempColumn = ref new ColumnDefinition();
+	//	tempColumn->Width = 100;
+	//	PageGrid->ColumnDefinitions->Append(tempColumn);
+	//}
+
+	////3. rectangle을 채워넣어본다. 
+	//for (int i = 0; i < 8; i++) {
+	//	for (int j = 0; j < 8; j++) {
+	//		//1. rectangle 채우기 
+	//		makeRectangle(PageGrid, 100, 100, i, j);
+	//	}
+	//}
+
+}
+
+void UWPPenAppDraft::GridContent::makeGridArray(int rowNum, int columnNum, int rectWidth) {
+	//1. 8개의 행을 그린다. 
+	for (int i = 0; i < rowNum; i++) {
+		RowDefinition^ tempRow = ref new RowDefinition();
+		tempRow->Height = rectWidth;
+		PageGrid->RowDefinitions->Append(tempRow);
+	}
+
+	//2. 8개의 열을 그린다. 
+	for (int i = 0; i < columnNum; i++) {
+		ColumnDefinition^ tempColumn = ref new ColumnDefinition();
+		tempColumn->Width = rectWidth;
+		PageGrid->ColumnDefinitions->Append(tempColumn);
+	}
+
+	//3. rectangle을 채워넣어본다. 
+	for (int i = 0; i < rowNum; i++) {
+		for (int j = 0; j < columnNum; j++) {
+			//1. rectangle 채우기 
+			makeRectangle(PageGrid, rectWidth, rectWidth, i, j);
+		}
+	}
+}
+void UWPPenAppDraft::GridContent::makeButton(Grid^ parentGrid, int horizontal, int vertical, int width, int height, int rowIdx, int columnIdx) {
+	Button^ tempButton = ref new Button();
+	tempButton->SetValue(HorizontalAlignmentProperty, horizontal);
+	tempButton->SetValue(VerticalAlignmentProperty, vertical);
+	tempButton->Padding = 0;
+	tempButton->Width = width;
+	tempButton->Height = height;
+	tempButton->SetValue(PageGrid->ColumnProperty, columnIdx);
+	tempButton->SetValue(PageGrid->RowProperty, rowIdx);
+
+	//tempButton->SetValue(this->VisibilityProperty, 0);
+	parentGrid->Children->Append(tempButton);
+}
+
+void  UWPPenAppDraft::GridContent::makeRectangle(Grid^ parentGrid, int width, int height, int rowIdx, int columnIdx) {
+	/*Rectangle^ tempRect = ref new Rectangle();
+	tempRect->Height = width;
+	tempRect->Width = height;
+	tempRect->StrokeThickness = 1;
+	tempRect->Stroke = ref new SolidColorBrush(Windows::UI::Colors::Gray);
+	tempRect->Fill = ref new SolidColorBrush(Windows::UI::Colors::Transparent);
+	tempRect->SetValue(PageGrid->ColumnProperty, columnIdx);
+	tempRect->SetValue(PageGrid->RowProperty, rowIdx);
+	tempRect->AllowDrop = true;
+	tempRect->Drop += ref new Windows::UI::Xaml::DragEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_Drop);
+	tempRect->PointerEntered += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerEntered);
+	tempRect->PointerPressed += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerPressed);
+	tempRect->PointerReleased += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerReleased);
+	tempRect->DragEnter += ref new Windows::UI::Xaml::DragEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_DragEnter);
+	parentGrid->Children->Append(tempRect);*/
+
+	Rectangle^ tempRect = ref new Rectangle();
+	tempRect->Height = width;
+	tempRect->Width = height;
+	tempRect->StrokeThickness = 0.5;
+	tempRect->Stroke = ref new SolidColorBrush(Windows::UI::Colors::Gray);
+	tempRect->Fill = ref new SolidColorBrush(Windows::UI::Colors::Transparent);
+	auto dasharray = ref new DoubleCollection;
+	dasharray->Append(5);
+	tempRect->StrokeDashArray = dasharray;
+	tempRect->SetValue(PageGrid->ColumnProperty, columnIdx);
+	tempRect->SetValue(PageGrid->RowProperty, rowIdx);
+	tempRect->AllowDrop = true;
+	tempRect->Drop += ref new Windows::UI::Xaml::DragEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_Drop);
+	tempRect->PointerEntered += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerEntered);
+	tempRect->PointerPressed += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerPressed);
+	tempRect->PointerReleased += ref new Windows::UI::Xaml::Input::PointerEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_PointerReleased);
+	tempRect->DragEnter += ref new Windows::UI::Xaml::DragEventHandler(this, &UWPPenAppDraft::GridContent::Rectangle_DragEnter);
+	parentGrid->Children->Append(tempRect);
+}
+
+
+
+
+
+void UWPPenAppDraft::GridContent::Rectangle_PointerEntered(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e)
+{
+	auto columnIdx = ((Rectangle^)sender)->GetValue(PageGrid->ColumnProperty);
+	auto rowIdx = ((Rectangle^)sender)->GetValue(PageGrid->RowProperty);
+
+	curColumnIdx = safe_cast<int>(columnIdx);
+	curRowIdx = safe_cast<int>(rowIdx);
+	/*String^ s1 = curColumnIdx + "  " + curRowIdx;
+	mainTextBox->Text = s1;*/
+
+	
+}
+
+
+
+void UWPPenAppDraft::GridContent::Rectangle_DragEnter(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e)
+{
+	auto columnIdx = ((Rectangle^)sender)->GetValue(PageGrid->ColumnProperty);
+	auto rowIdx = ((Rectangle^)sender)->GetValue(PageGrid->RowProperty);
+
+	curColumnIdx = safe_cast<int>(columnIdx);
+	curRowIdx = safe_cast<int>(rowIdx);
+	
+	//String^ s1 = curRowIdx + "  " + curColumnIdx;
+	//mainTextBox->Text = s1;
+}
+
+
+void UWPPenAppDraft::GridContent::PageGrid_DragOver(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e)
+{
+
+	e->DragUIOverride->Caption = "여기 놓으세요.";
+	e->DragUIOverride->IsGlyphVisible = false;
+	e->AcceptedOperation = DataPackageOperation::Copy;
+	
+}
+
+void UWPPenAppDraft::GridContent::PageGrid_Drop(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e)
+{
+	
+	makeButton(PageGrid, 0, 0, 20, 20, curRowIdx, curColumnIdx);
+	makeButton(PageGrid, 0, 2, 20, 20, curRowIdx, curColumnIdx);
+	makeButton(PageGrid, 2, 0, 20, 20, curRowIdx, curColumnIdx);
+	makeButton(PageGrid, 2, 2, 20, 20, curRowIdx, curColumnIdx);
+	PageGrid->UpdateLayout();
+
+}
+
+
+
+
+
+
+
+
+
+void UWPPenAppDraft::GridContent::Rectangle_PointerReleased(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e)
+{
+	
+
+}
+
+void UWPPenAppDraft::GridContent::PageGrid_PointerEntered(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e)
+{
+	
+}
+
+void UWPPenAppDraft::GridContent::Rectangle_Drop(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e)
+{
+
+}
+
+void  UWPPenAppDraft::GridContent::Rectangle_PointerPressed(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e)
+{
+
+}
+
---
+++ UWPPenAppDraft/GridContent.xaml.h
@@ -0,0 +1,52 @@
+﻿//
+// GridContent.xaml.h
+// Declaration of the GridContent class
+//
+
+#pragma once
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::UI::Xaml::Shapes;
+using namespace Windows::UI;
+#include "GridContent.g.h"
+
+namespace UWPPenAppDraft
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class GridContent sealed
+	{
+	public:
+		GridContent();
+
+
+	private:
+		int curRowIdx, curColumnIdx;
+		
+		void makeButton(Grid^ parentGrid, int horizontal, int vertical, int width, int height, int rowIdx, int columnIdx);
+		void makeRectangle(Grid^ parentGrid, int width, int height, int rowIdx, int columnIdx);
+		void makeGridArray(int rowNum, int columnNum, int rectWidth);
+
+		void Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Rectangle_PointerPressed(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+		void Rectangle_PointerEntered(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+		
+		void PageGrid_DragOver(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e);
+		void Rectangle_PointerReleased(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+		void PageGrid_Drop(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e);
+		void Rectangle_DragEnter(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e);
+		void PageGrid_PointerEntered(Platform::Object^ sender, Windows::UI::Xaml::Input::PointerRoutedEventArgs^ e);
+		void Rectangle_Drop(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e);
+		
+	};
+}
